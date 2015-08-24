c59168b770c38d526f91885baa0c7a71411110ef
Aakash Sharma <aakashjsharma@gmail.com>
2015-08-24 14:12:26 +0530

    Add urlsList in lists.py, Modify other functions also

--- voterDemographicsAnalysis/lists.py
+++ voterDemographicsAnalysis/lists.py
@@ -29,7 +29,7 @@ def fileNames(end):
 	Returns 3 lists. Each is a list of file Names
 	list1 : list of pdf file Names
 	list2 : list of file names converted from pdf to text
-	list3 : list of fileNames after first extraction using regex
+	list3 : list of file Names after first extraction using regex
 	'''
 	numberList = generateNumberList()
 	#[:3] for 3 files only, use [:] for files upto 399
@@ -41,16 +41,78 @@ def fileNames(end):
 	for number in numbers:
 		pdfsNameList.append("AC174"+ number +".pdf")
 		txtConvertsNameList.append("AC174"+ number +".txt")
-		firstExtractsNameList.append("AC174"+ number +"b.txt")		    	
+		firstExtractsNameList.append("AC174"+ number +"b.txt")
 
    	return pdfsNameList, txtConvertsNameList, firstExtractsNameList
 
 
+def pdfsNamesList(startIndex, endIndex):
+	'''(int, int) -> list
+	Takes the number of files in the list as input (start, end)
+	Returns list of pdf file Names
+	'''
+	numberList = generateNumberList()
+	#[:3] for 3 files only, use [:] for files upto 399
+	numbers = numberList[startIndex:endIndex]
+	list = []
+
+	for number in numbers:
+		list.append("AC174"+ number +".pdf")		
+
+   	return list
+
+
+def txtConvertsNamesList(startIndex, endIndex):
+	'''(int, int) -> list
+	Takes the number of files in the list as input (start, end)
+	Returns list of file names converted from pdf to text
+	'''
+	numberList = generateNumberList()	
+	numbers = numberList[startIndex:endIndex]
+	list = []		
+
+	for number in numbers:
+		list.append("AC174"+ number +".txt")
+
+   	return list
+
+
+def firstExtractsNamesList(startIndex, endIndex):
+	'''(int, int) -> list
+	Takes the number of files in the list as input (start, end)
+	Returns list of file Names after first extraction using regex
+	'''
+	numberList = generateNumberList()	
+	numbers = numberList[startIndex:endIndex]
+	list = []		
+
+	for number in numbers:
+		list.append("AC174"+ number +"b.txt")
+
+   	return list
+
+
+def urlsList(startIndex,endIndex):	
+	l = pdfsNamesList(startIndex,endIndex)	
+	list = []
+	pdfUrl = "http://ceokarnataka.kar.nic.in/FinalRoll_2015/English/WOIMG/AC174/"
+	
+	for pdfFileName in l:
+		list.append(pdfUrl+pdfFileName)
+
+	return list
+
+
 def main():
-	pdfsNameList, txtConvertsNameList, firstExtractsNameList = fileNames(2)
-	print pdfsNameList
-	print txtConvertsNameList
-	print firstExtractsNameList
+	#pdfsNamesList, txtConvertsNamesList, firstExtractsNamesList = fileNames(2)
+	l1 = pdfsNamesList(0,3)
+	l2 = txtConvertsNamesList(0,3)
+	l3 = firstExtractsNamesList(0,3)
+	l4 = urlsList(0,3)
+	print l1
+	print l2
+	print l3
+	print l4
 
 if __name__ == '__main__':
 	main()
